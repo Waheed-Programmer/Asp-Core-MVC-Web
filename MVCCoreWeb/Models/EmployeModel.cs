448e0950a12703220e095eea8c567a2abfba11c2
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-01-01 18:31:06 +0500

    Add data Annotations in model forr client side validations

--- MVCCoreWeb/Models/EmployeModel.cs
+++ MVCCoreWeb/Models/EmployeModel.cs
@@ -12,21 +12,27 @@ namespace MVCCoreWeb.Models
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Date is Required")]
+        [DataType(DataType.Date)]
         public DateTime? DateBirth { get; set; }
 
         [Required(ErrorMessage = "Email is Required")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Mobile No is Required")]
+        [DataType(DataType.PhoneNumber)]
         public string Mobile { get; set; }
 
         [Required(ErrorMessage = "Address is Required")]
+        [DataType(DataType.MultilineText)]
         public string Address { get; set; }
 
         [Required(ErrorMessage = "Salary is Required")]
+        
         public decimal Salary { get; set; }
 
         [Required(ErrorMessage = "Website is Required")]
+        [DataType(DataType.Url)]
         public string Website { get; set; }
 
     }
