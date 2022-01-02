f5e09486ed581de53a484bb26c1e3ea9a2096dc0
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-01-02 10:43:58 +0500

    Create  action method and view to save data into database

--- MVCCoreWeb/Controllers/CrudDB.cs
+++ MVCCoreWeb/Controllers/CrudDB.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MVCCoreWeb.Models.Database;
+using MVCCoreWeb.ViewModel;
 
 namespace MVCCoreWeb.Controllers
 {
     public class CrudDB : Controller
     {
-        public IActionResult Index()
+       private TestDBContext db = new TestDBContext();
+
+        [HttpGet]
+        public IActionResult List()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ViewModelProduct p)
+        {
+            return View();
+        }
+
     }
 }
---
+++ MVCCoreWeb/ViewModel/ViewModelProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreWeb.ViewModel
+{
+    public class ViewModelProduct
+    {
+        public int ProductId { get; set; }
+       
+        public string ProductName { get; set; }
+       
+        public decimal? Price { get; set; }
+
+        public int? UserId { get; set; }
+    }
+}
