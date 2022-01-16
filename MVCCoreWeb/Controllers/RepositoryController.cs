aadd7b0e895f880a864a886df9ee63030cfcbe22
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-01-16 09:23:38 +0500

    Using Data dependency get list data from data base

--- MVCCoreWeb/Controllers/RepositoryController.cs
+++ MVCCoreWeb/Controllers/RepositoryController.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MVCCoreWeb.Interfaces;
 
 namespace MVCCoreWeb.Controllers
 {
     public class RepositoryController : Controller
     {
+        private IUser _userServices;
+        public RepositoryController(IUser userServices)
+        {
+           this._userServices = userServices;
+        }
         public IActionResult Index()
         {
-            return View();
+            dynamic Obj = new ExpandoObject();
+            Obj.userList = _userServices.GetUsers();
+            return View(Obj);
         }
     }
 }
